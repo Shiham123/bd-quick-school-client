7de3ed949883a4c857431a3ad2346b56752178a9
Shiham123 <s36651786@gmail.com>
2024-01-16 21:00:24 +0600

    three section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '../../Components/Banner/Banner';
+import Faq from '../Faq/Faq';
 import './hover.css';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Faq />
     </div>
   );
 };

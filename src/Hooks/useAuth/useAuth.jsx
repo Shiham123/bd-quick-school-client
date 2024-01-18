47f68b4823a537f51c41de4b79210be34c3f1c8c
tonoy3125 <shaifshajedt@gmail.com>
2024-01-18 18:42:11 +0600

    useaxiospublic hook added commit

--- src/Hooks/useAuth/useAuth.jsx
+++ src/Hooks/useAuth/useAuth.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import { AuthContext } from './../Providers/Authprovider';
+import { AuthContext } from './../../Providers/Authprovider';
+
 
 const useAuth = () => {
     const auth = useContext(AuthContext);

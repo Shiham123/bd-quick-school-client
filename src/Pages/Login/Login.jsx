d2ee0fc51b447bf77cef3d72d974c70d77f8b70d
tonoy3125 <shaifshajedt@gmail.com>
2024-01-18 13:04:21 +0600

    login email input added commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,17 +1,31 @@
-
+import { MdOutlineEmail } from "react-icons/md";
 const Login = () => {
-    
-    
+
+
     return (
         <div>
             <div className="flex justify-center items-center font-[sans-serif] text-[#333] h-full min-h-screen p-4" style={{ backgroundImage: 'url(https://i.ibb.co/Jspy7Nq/register.png)', backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
                 <div className="max-w-md w-full mx-auto">
-                    <form  className="bg-opacity-70 bg-white rounded-2xl p-6 shadow-xl">
+                    <form className="bg-opacity-70 bg-white rounded-2xl p-6 shadow-xl">
                         <div className="mb-10">
-                            <h3 className="text-3xl font-extrabold">Sign in</h3>
+                            <h3 className="text-3xl font-extrabold ">Sign in</h3>
                         </div>
+                        {/* email  */}
+                        <div>
+                            <div className="relative flex items-center">
+                                <MdOutlineEmail className="text-2xl absolute right-3 top-1/2 transform -translate-y-1/2" />
+                                <input
+                                    
+                                    type="email"
 
-                        
+                                    className="bg-transparent w-full text-sm border-b border-[#333] px-1 lg:px-2 py-3 outline-none placeholder:text-[#333]"
+                                    placeholder="Enter email"
+                                />
+                            </div>
+                           
+                        </div>
+                       
+
                     </form>
                 </div>
             </div>

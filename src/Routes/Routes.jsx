6446b73a453f6062d9434fa8acd9c24ca9be3619
Shiham123 <s36651786@gmail.com>
2024-01-18 17:46:02 +0600

    readme file text removed and .prettierrc file added in git ignore

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,13 +2,21 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../Layout/Root/MainLayout';
 import ErrorElement from '../Layout/ErrorElement/ErrorElement';
 import Home from '../Pages/Home/Home';
+// import CourseDetails from '../Pages/Courses/CourseDetails';
+import Register from '../Pages/Register/Register';
+import Login from '../Pages/Login/Login';
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
     errorElement: <ErrorElement />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      // { path: 'Couredetails', element: <CourseDetails /> },
+      { path: 'register', element: <Register /> },
+      { path: 'login', element: <Login /> },
+    ],
   },
 ]);
 

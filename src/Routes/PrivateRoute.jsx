bde8bacedd7e60cd4d738ebca31bb971fb061838
tonoy3125 <shaifshajedt@gmail.com>
2024-01-18 19:12:28 +0600

    private route added commit

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from './../Hooks/useAuth/useAuth';
+
+
+
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth()
+    const location = useLocation()
+
+    if (loading) {
+        return <p className="text-center"><span className="loading loading-spinner loading-lg"></span></p>
+    }
+
+    if (user) {
+        return children
+    }
+    return <Navigate state={location.pathname} to="/login"></Navigate>
+};
+
+export default PrivateRoute;

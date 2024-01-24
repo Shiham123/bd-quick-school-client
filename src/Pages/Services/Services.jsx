d35c3cf1645a3b7594e980e44370eb5a0e163b2c
sanjidatanha09 <tanha.sanjida999@gmail.com>
2024-01-24 20:50:41 +0600

    Responsive

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -16,23 +16,6 @@ const Services = () => {
   return (
     <div>
       <Services_banner></Services_banner>
-<<<<<<< HEAD
-      <div className="py-16 w-[90%] md:w-[90%] lg:w-[75%] mx-auto text-white">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-x-10 gap-y-10 py-10">
-          {services.map((service, idx) => (
-            <div
-              key={idx}
-              className="cursor-pointer bg-white text-black rounded-xl shadow-2xl border-b-8 border-borderColorOne transition duration-600 ease-in-out hover:scale-105"
-            >
-              <img className="w-[100%] rounded-t-xl border-t" src={service.image} alt="" />
-              <div className="p-4">
-                <h2 className="text-3xl font-bold py-6">{service.title}</h2>
-                <p className="text-xl">{service.description}</p>
-                <p className="py-6 text-2xl font-bold">Price: ${service.price}</p>
-                <button className="bg-servicesBg text-white font-bold py-2 px-4 border-b-4 border-borderColorOne hover:border-black rounded">
-                  Explore
-                </button>
-=======
       <div className="py-16 w-[90%] md:w-[75%] mx-auto text-white">
         <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-x-10 gap-y-10 py-10">
           {services.map((service) => (
@@ -48,7 +31,6 @@ const Services = () => {
                     Explore
                   </button>
                 </div>
->>>>>>> f949ec3da7223bc480451e788f802c37ee18e37c
               </div>
             </Link>
           ))}

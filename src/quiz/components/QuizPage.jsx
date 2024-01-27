695263bd3ab8bf3408a96acb0bd150ad7bd4f0e5
Shiham123 <s36651786@gmail.com>
2024-01-27 19:01:37 +0600

    answer show and shared folder added for basic quiz

--- src/quiz/components/QuizPage.jsx
+++ src/quiz/components/QuizPage.jsx
@@ -1,25 +1,38 @@
+import QuizHeading from '../shared/QuizHeading';
+
 const QuizPage = (props) => {
   const { quiz, showQuiz, question } = props;
 
   return (
     <section style={{ display: `${showQuiz ? 'block' : 'none'}` }}>
-      <div className="flex flex-col border-2 m-20 px-10 py-10 rounded-lg">
-        <h5 className="font-lora text-white text-2xl">
-          {' '}
-          <span className="font-bold">Question :</span> {question?.question}
-        </h5>
+      <div className="flex flex-col justify-center items-center">
+        <QuizHeading headingText="This is very basic quiz for admission" />
 
-        <div>
-          {question?.options?.map((item, index) => {
-            return (
-              <button
-                className="flex text-white font-lora font-light text-xl border-[1px] w-full gap-4 my-4 px-8 py-6"
-                key={index}
-              >
-                {item}
-              </button>
-            );
-          })}
+        {/* all quiz */}
+
+        <div className="flex flex-col border-2 m-20 px-10 py-10 rounded-lg w-4/5">
+          <div className="flex justify-between items-center">
+            <h5 className="font-lora text-white text-2xl">
+              {' '}
+              <span className="font-bold">Question :</span> {question?.question}
+            </h5>
+            <span className="text-white font-lora font-light text-2xl">
+              {quiz.indexOf(question) + 1} / {quiz.length}
+            </span>
+          </div>
+
+          <div>
+            {question?.options?.map((item, index) => {
+              return (
+                <button
+                  className="flex text-white font-lora font-light text-xl border-[1px] w-full gap-4 my-4 px-8 py-6"
+                  key={index}
+                >
+                  {item}
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
--- src/quiz/components/StartQuiz.jsx
+++ src/quiz/components/StartQuiz.jsx
@@ -1,12 +1,12 @@
+import QuizHeading from '../shared/QuizHeading';
+
 const StartQuiz = (props) => {
   const { showStart, startQuiz } = props;
 
   return (
-    <section>
+    <section style={{ display: `${showStart ? 'block' : 'none'}` }}>
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-2xl md:text-3xl lg:text-5xl font-cinzel font-semibold tracking-widest py-12 uppercase">
-          This is basic quiz for admission
-        </h1>
+        <QuizHeading headingText="This is basic quiz for admission" />
         <button
           onClick={startQuiz}
           className="bg-transparent text-white font-poppins font-semibold tracking-widest capitalize border-[1px] border-white px-8 py-4 rounded-lg text-xl hover:scale-110 duration-300 transition-all"

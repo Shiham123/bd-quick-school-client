592f5955b1163c3fa0a2c8baa71d33681b0853dd
Shiham123 <s36651786@gmail.com>
2024-01-29 17:55:28 +0600

    modal design running

--- src/quiz/shared/QuizModal.jsx
+++ src/quiz/shared/QuizModal.jsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from 'react';
 import Lottie from 'react-lottie';
 import QuizAnimationJson from '../../assets/json/Animation - 1706526672114.json';
+import QuizButton from './QuizButton';
 
 const QuizModal = (props) => {
   const { closeModal } = props;
+
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth * 0.5,
+    height: window.innerHeight * 0.5,
+  });
 
   const defaultOptions = {
     loop: true,
@@ -11,14 +18,27 @@ const QuizModal = (props) => {
     rendererSettings: { preserveAspectRatio: 'xMidYMid slice' },
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({ width: window.innerWidth * 0.5, height: window.innerHeight * 0.5 });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="fixed right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll z-50 flex justify-center items-center bg-black/50">
-      <div className="bg-white w-1/2 h-1/2 ">
-        <h1 className="text-black">This is quiz modal</h1>
-        <Lottie options={defaultOptions} height={200} width={200} />
-        <button className="text-black" onClick={closeModal}>
-          Close modal
-        </button>
+    <div className="fixed right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll z-50 flex justify-center items-center bg-black/50 rounded-lg">
+      <div className="bg-gradient-to-tr from-[#8a2d48] via-[#413f43] to-[#172A3A] w-full h-full lg:w-[70vw] lg:h-[70vh] flex flex-col justify-center items-center rounded-lg">
+        <Lottie options={defaultOptions} height={dimensions.height} width={dimensions.width} />
+        <h1 className="text-white font-lora text-xl md:text-3xl lg:text-5xl font-semibold">
+          This is quiz modal
+        </h1>
+
+        <QuizButton btnText="Close modal" onClick={closeModal} />
       </div>
     </div>
   );

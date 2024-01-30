c1bf85fe9870bb2605a60bb089653dcce3b5645f
sanjidatanha09 <tanha.sanjida999@gmail.com>
2024-01-30 21:24:19 +0600

    FAQ section comment added

--- src/Pages/Faq/Faq.jsx
+++ src/Pages/Faq/Faq.jsx
@@ -72,6 +72,7 @@ const Faq = () => {
             </AccordionDetails>
           </Accordion>
           {/* ############# */}
+          {/* ############# */}
           <Accordion>
             <AccordionSummary
               expandIcon={<ArrowDropDownIcon />}

bbb269663356a7281e8f6c582a45a24d75561c52
Shiham123 <s36651786@gmail.com>
2024-02-02 11:13:43 +0600

    admin fixing

--- src/Hooks/useAdmin/useAdmin.jsx
+++ src/Hooks/useAdmin/useAdmin.jsx
@@ -1,20 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
-import UseAxiosSecure from "../UseAxiosSecure/UseAxiosSecure";
-import useAuth from "../useAuth/useAuth";
+import { useQuery } from '@tanstack/react-query';
+import UseAxiosSecure from '../UseAxiosSecure/UseAxiosSecure';
+import useAuth from '../useAuth/useAuth';
 
 const UseAdmin = () => {
-    const axiosSecure = UseAxiosSecure()
-    const { user } = useAuth()
-    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
-        queryKey: [user?.email, 'isAdmin'],
-        enabled: !!user?.email && !!localStorage.getItem('access-token'),
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/api/v1/user/admin/${user?.email}`)
-            console.log(res.data)
-            return res.data?.admin
-        }
-    })
-    return [isAdmin, isAdminLoading]
+  const axiosSecure = UseAxiosSecure();
+  const { user } = useAuth();
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    queryKey: [user?.email, 'isAdmin'],
+    enabled: !!user?.email && !!localStorage.getItem('access-token'),
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/api/v1/user/admin/${user?.email}`);
+      return res.data?.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
 };
 
 export default UseAdmin;

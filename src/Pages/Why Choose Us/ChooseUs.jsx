281c399fed535ff7b36db55e7c813a2d36c80012
tonoy3125 <shaifshajedt@gmail.com>
2024-02-05 02:58:00 +0600

    why choose us Components 4th step design added

--- src/Pages/Why Choose Us/ChooseUs.jsx
+++ src/Pages/Why Choose Us/ChooseUs.jsx
@@ -1,12 +1,13 @@
 import { FaGraduationCap } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaIdCard } from "react-icons/fa";
+import { TbAdjustmentsHorizontal } from "react-icons/tb";
 
 const ChooseUs = () => {
     return (
         <div className="max-w-7xl mx-auto mb-20">
             <h1 className="text-4xl font-cinzel font-bold text-white text-center mb-20">Why choose us</h1>
-            <div className="font-lora grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="font-lora grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-16">
                 <div className="flex items-start gap-4">
                     <div className=" py-5 px-5 border rounded-full bg-[#DD9933]">
                         <FaGraduationCap className="text-4xl text-white" />
@@ -32,6 +33,15 @@ const ChooseUs = () => {
                     <div className="lg:w-72">
                         <h3 className="text-white text-2xl mb-5">Hearts</h3>
                         <p className="text-white text-base">Learning with MasterStudy is fun and addictive. Earn points for correct answers, race against the clock.</p>
+                    </div>
+                </div>
+                <div className="flex items-start gap-4">
+                    <div className=" py-5 px-5 border rounded-full bg-[#0F78EF]">
+                        <TbAdjustmentsHorizontal className="text-4xl text-white" />
+                    </div>
+                    <div className="lg:w-72">
+                        <h3 className="text-white text-2xl mb-5">Wide range of Courses</h3>
+                        <p className="text-white text-base">Instantly see which answers you get correct. When you miss a challenge, we’ll quickly show you how to improve. Start with Professionals.</p>
                     </div>
                 </div>
             </div>

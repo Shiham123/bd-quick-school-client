10469e3fd6929670b55c4c298d49e4a42ade3c19
Shiham123 <s36651786@gmail.com>
2024-02-04 11:24:14 +0600

    working on it

--- src/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -13,10 +13,10 @@ const useAxiosSecure = () => {
   axiosSecure.interceptors.request.use(
     (config) => {
       const token = localStorage.getItem('access-token');
+      console.log(token);
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
-
     (error) => {
       return Promise.reject(error);
     }

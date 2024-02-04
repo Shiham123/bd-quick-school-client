017235d5452fae720a73d2f20b2541f3800dd70a
tonoy3125 <shaifshajedt@gmail.com>
2024-02-05 00:45:33 +0600

    why choose us Components 2nd step design added

--- src/Pages/Why Choose Us/ChooseUs.jsx
+++ src/Pages/Why Choose Us/ChooseUs.jsx
@@ -1,10 +1,12 @@
 import { FaGraduationCap } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { FaIdCard } from "react-icons/fa";
 
 const ChooseUs = () => {
     return (
-        <div className="container mx-auto mb-20">
+        <div className="max-w-7xl mx-auto mb-20">
             <h1 className="text-4xl font-cinzel font-bold text-white text-center mb-20">Why choose us</h1>
-            <div className="font-lora">
+            <div className="font-lora grid grid-cols-1 md:grid-cols-3 gap-5">
                 <div className="flex items-start gap-4">
                     <div className=" py-5 px-5 border rounded-full bg-[#DD9933]">
                         <FaGraduationCap className="text-4xl text-white" />
@@ -14,6 +16,16 @@ const ChooseUs = () => {
                         <p className="text-white text-base">Each lesson includes a variety of speaking, listening, translation and multiple choice challenge.</p>
                     </div>
                 </div>
+                <div className="flex items-start gap-4">
+                    <div className=" py-5 px-5 border rounded-full bg-[#18BC6D]">
+                        <FaBook className="text-4xl text-white" />
+                    </div>
+                    <div className="lg:w-72">
+                        <h3 className="text-white text-2xl mb-5">Quality control</h3>
+                        <p className="text-white text-base">MasterStudy motivates you to stay on track by recording how many days in a row you spend learning a language.</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );

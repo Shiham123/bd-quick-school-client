ff03b900474a0ccb2e9c8dba86680e71b9812f53
Shiham123 <s36651786@gmail.com>
2024-02-04 18:47:39 +0600

    working

--- src/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure/UseAxiosSecure.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosSecure = axios.create({
-  // baseURL: 'https://quiz-school-server.vercel.app',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://quiz-school-server.vercel.app',
+  // baseURL: 'http://localhost:5000',
 });
 
 const useAxiosSecure = () => {
@@ -20,7 +20,6 @@ const useAxiosSecure = () => {
   );
   axiosSecure.interceptors.response.use(
     (response) => {
-      console.log(response);
       return response;
     },
     (error) => {
--- src/Hooks/useAxiosPublic/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  // baseURL: 'https://quiz-school-server.vercel.app',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://quiz-school-server.vercel.app',
+  // baseURL: 'http://localhost:5000',
 });
 
 const useAxiosPublic = () => {

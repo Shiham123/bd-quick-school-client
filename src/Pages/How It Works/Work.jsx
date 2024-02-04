e441f0e1b167dea8984474487e33355d90a568c7
tonoy3125 <shaifshajedt@gmail.com>
2024-02-04 18:54:59 +0600

    how it work design 2nd step

--- src/Pages/How It Works/Work.jsx
+++ src/Pages/How It Works/Work.jsx
@@ -10,6 +10,11 @@ const Work = () => {
                     <h2 className="text-2xl font-bold text-white text-center mt-4 mb-6">Sign up</h2>
                     <p className="text-white/70 text-center">Our services and collections adhere to cutting-edge solutions in the design world.</p>
                 </div>
+                <div className="w-[350px] mx-auto">
+                    <img src="https://i.ibb.co/NScDsqv/21-layers-123x123-1.png" className="mx-auto" alt="" />
+                    <h2 className="text-2xl font-bold text-white text-center mt-4 mb-6">Select course</h2>
+                    <p className="text-white/70 text-center">We have everything for effective learning and delivery of assignments by students.</p>
+                </div>
                 
 
             </div>

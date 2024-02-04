8222f6b5c9aa88923aa83517e69ca883e196c699
tonoy3125 <shaifshajedt@gmail.com>
2024-02-04 18:29:14 +0600

    how it work component added and design 1st step

---
+++ src/Pages/How It Works/Work.jsx
@@ -0,0 +1,20 @@
+
+
+const Work = () => {
+    return (
+        <div className="max-w-7xl mx-auto mb-32">
+            <h1 className="text-white text-4xl font-cinzel font-semibold text-center mb-10">How it works?</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3">
+                <div className="w-[350px] mx-auto">
+                    <img src="https://i.ibb.co/4mFY1sX/25-layers.png" className="mx-auto" alt="" />
+                    <h2 className="text-2xl font-bold text-white text-center mt-4 mb-6">Sign up</h2>
+                    <p className="text-white/70 text-center">Our services and collections adhere to cutting-edge solutions in the design world.</p>
+                </div>
+                
+
+            </div>
+        </div>
+    );
+};
+
+export default Work;

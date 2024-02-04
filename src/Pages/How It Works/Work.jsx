62f4283b71c2802d714d38e2ee05318a2110bcfe
tonoy3125 <shaifshajedt@gmail.com>
2024-02-04 18:56:59 +0600

    how it work component design 3rd step

--- src/Pages/How It Works/Work.jsx
+++ src/Pages/How It Works/Work.jsx
@@ -15,7 +15,11 @@ const Work = () => {
                     <h2 className="text-2xl font-bold text-white text-center mt-4 mb-6">Select course</h2>
                     <p className="text-white/70 text-center">We have everything for effective learning and delivery of assignments by students.</p>
                 </div>
-                
+                <div className="w-[350px] mx-auto">
+                    <img src="https://i.ibb.co/Jn9Fbmy/28-layers.png" className="mx-auto" alt="" />
+                    <h2 className="text-2xl font-bold text-white text-center mt-4 mb-6">Start Learning</h2>
+                    <p className="text-white/70 text-center">After completing the course, you will receive a certificate confirming your knowledge.</p>
+                </div>
 
             </div>
         </div>

30f46fd8a16e5c63074c25b701ec00ddba077455
tonoy3125 <shaifshajedt@gmail.com>
2024-02-05 18:03:48 +0600

    courses type design 2nd step design added

--- src/Pages/Courses_Type/CoursesType.jsx
+++ src/Pages/Courses_Type/CoursesType.jsx
@@ -3,13 +3,22 @@
 const CoursesType = () => {
     return (
         <div className="max-w-7xl mx-auto">
-            <div>
+            <div className="flex items-center justify-center gap-24">
                 <div className="font-lora flex items-start gap-10">
                     <img src="https://i.ibb.co/tpSndm7/icons8-training-64.png" className="w-[103px] mt-3" alt="" />
-                    <div className="lg:w-[300px] border-r-2">
+                    <div className="lg:w-[300px] ">
                         <h2 className="text-3xl text-white font-semibold mb-5">View Courses</h2>
                         <p className="text-base text-white mb-5">Here you can find any courses that suit you. Simply navigate and select the course.</p>
-                        <button className="px-9 py-3 bg-[#3FA385] text-lg rounded-full text-white">Start Searching</button>
+                        <button className="px-9 py-3 bg-[#3FA385] text-lg rounded-full text-white uppercase">Start Searching</button>
+                    </div>
+                </div>
+                <div className="divider divider-accent lg:divider-horizontal"></div>
+                <div className="font-lora flex items-start gap-10">
+                    <img src="https://i.ibb.co/G2xW95S/icons8-deal-64.png" className="w-[103px] mt-3" alt="" />
+                    <div className="lg:w-[300px]">
+                        <h2 className="text-3xl text-white font-semibold mb-5">Premium Courses</h2>
+                        <p className="text-base text-white mb-5">Become one of our exclusive members and get more exquisite and advanced options.</p>
+                        <button className="px-9 py-3 bg-[#3FA385] text-lg rounded-full text-white uppercase">Get Premium</button>
                     </div>
                 </div>
             </div>

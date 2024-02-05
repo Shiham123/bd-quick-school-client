7b99a06d76c0c47a7f7cc697cbc3d36e414c05f4
tonoy3125 <shaifshajedt@gmail.com>
2024-02-05 23:30:42 +0600

    admin route updated

--- src/DashBoard/DashBoardAdmin/DashBoardAdmin.jsx
+++ src/DashBoard/DashBoardAdmin/DashBoardAdmin.jsx
@@ -12,7 +12,7 @@ const DashBoardAdmin = () => {
             {/* Dashboard Route */}
             <ul className="mb-8 text-sm">
                 <li>
-                    <Link to="/dashboard"
+                    <Link to="/dashboard/charts"
                         className="flex items-center gap-[14px] px-8 py-4 text-gray-100 bg-blue-600 dark:text-gray-200 dark:bg-blue-500 dark:hover:bg-gray-700 hover:bg-blue-500">
                         <GrDashboard className="text-xl" />
                         <span className="text-base font-semibold">Dashboard</span>
@@ -26,26 +26,42 @@ const DashBoardAdmin = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/manageusers"
+                    <Link to="/dashboard/addcourses"
                         className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">
-                        <FaUsers className="text-xl" />
-                        <span className="text-base font-semibold"> Manage Users </span>
+                        <ImProfile className="text-xl" />
+                        <span className="text-base font-semibold">Add Courses </span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/manageusers"
+                    <Link to="/dashboard/uploadcontent"
+                        className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">
+                        <ImProfile className="text-xl" />
+                        <span className="text-base font-semibold"> Upload Content</span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/dashboard/soldcourses"
                         className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">
                         <IoBookOutline className="text-xl" />
                         <span className="text-base font-semibold"> Sold Courses </span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/dashboard/manageusers"
+                    <Link to="/dashboard/managepayment"
                         className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">
                         <FaCcMastercard className="text-xl" />
                         <span className="text-base font-semibold"> Manage Payment </span>
                     </Link>
                 </li>
+                <li>
+                    <Link to="/dashboard/manageusers"
+                        className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">
+                        <FaUsers className="text-xl" />
+                        <span className="text-base font-semibold"> Manage Users </span>
+                    </Link>
+                </li>
+
                 <li>
                     <Link to="/dashboard/managereviews"
                         className="flex items-center gap-[14px] px-8 py-4 group dark:text-gray-400 dark:hover:bg-gray-700 hover:bg-gray-100">

dc3928fca5a6abd27661bf0b8afc407e634722c7
tonoy3125 <shaifshajedt@gmail.com>
2024-02-07 19:55:43 +0600

    add service route teacher image upload input design added

--- src/Components/AddServicesForm/AddServices.jsx
+++ src/Components/AddServicesForm/AddServices.jsx
@@ -61,7 +61,7 @@ const AddServices = () => {
           <div className="my-8">
             <Divider className=" text-2xl font-bold">Course Information</Divider>
           </div>
-          <fieldset className="grid grid-cols-4 gap-6  rounded-md shadow-sm dark:bg-gray-900">
+          <fieldset className="grid grid-cols-4 gap-6   dark:bg-gray-900">
             <div className="space-y-2 col-span-full lg:col-span-1">
               <div>
                 <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">Upload Course Image <span className="text-red-700">*</span></h2>
@@ -108,12 +108,48 @@ const AddServices = () => {
               </div>
 
 
+            </div>
 
-              <div className="col-span-full mt-5">
-                <input type="submit" value="Submit Courses" className="btn btn-block bg-[#4357AD] text-lg text-[#fff] hover:bg-[#154360] " />
+          </fieldset>
+          <div className="my-8">
+            <Divider className=" text-2xl font-bold">Teacher Information</Divider>
+          </div>
+
+          <fieldset className="grid grid-cols-4 gap-6   dark:bg-gray-900">
+            <div className="space-y-2 col-span-full lg:col-span-1">
+              <div>
+                <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">Upload Teacher Image <span className="text-red-700">*</span></h2>
+                <div className="flex items-center justify-center w-full">
+                  <label className="flex flex-col items-center justify-center w-full h-56 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer  dark:hover:bg-bray-800 dark:bg-gray-700  dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg className="w-8 h-8 mb-4 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                      </svg>
+                      <p className="mb-2 text-sm text-black"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                      <p className="text-xs text-black">SVG, PNG, JPG or GIF </p>
+                    </div>
+                    <input id="dropzone-file" type="file" {...register('imageFile', { required: true })} name='courseimage' />
+                  </label>
+                </div>
               </div>
             </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+              <div className="col-span-full lg:col-span-3">
+                <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">Teacher Name<span className="text-red-700">*</span></h2>
+                <input className="pt-4 pb-4 pl-2 md:p-4 w-full border border-black  text-base font-normal text-[#1B1A1A99] rounded" type="text" name="teachername" {...register('teachername', { required: true })} placeholder="Enter Teacher Name Here" id="" />
+              </div>
+              
+
+
+
+
+            </div>
+
           </fieldset>
+
+          <div className="col-span-full mt-5">
+            <input type="submit" value="Submit Courses" className="btn btn-block bg-[#4357AD] text-lg text-[#fff] hover:bg-[#154360] " />
+          </div>
         </form>
       </section>
       {/* <form onSubmit={handleSubmit(onSubmit)}>

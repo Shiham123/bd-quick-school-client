d198f209598384d7b88baab14b965d66fbf4938e
tonoy3125 <shaifshajedt@gmail.com>
2024-02-08 14:11:00 +0600

    table data added and design added commit

--- src/DashBoard/Manage Users/ManageUsers.jsx
+++ src/DashBoard/Manage Users/ManageUsers.jsx
@@ -51,7 +51,35 @@ const ManageUsers = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        {
+                            users.map((user, index) => <tr key={user._id} className="bg-blue-600 border-b border-blue-400 hover:bg-blue-500">
+                                <th scope="row" className="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
+                                    {index + 1}
+                                </th>
+                                <td className="px-6 py-4">
+                                    <img className="w-10 h-10 rounded-full" src={user.photoURL} alt="Jese image" />
+                                </td>
+                                <td className="px-6 py-4">
+                                    {user.name}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {user.email}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {user.phone}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {user.role}
+                                </td>
+                                <td className="px-6 py-4">
+                                    <button className="btn btn-outline text-white"><FaBan className="text-xl" /></button>
+                                </td>
+                                <td className="px-6 py-4">
+                                    <button className="btn btn-outline text-white"><RiDeleteBin5Line className="text-xl" /></button>
+                                </td>
+                            </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>

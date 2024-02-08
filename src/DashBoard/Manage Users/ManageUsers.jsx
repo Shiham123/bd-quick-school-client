dcbdaad71be876ef44b4cc76ff471a3bce8f288c
tonoy3125 <shaifshajedt@gmail.com>
2024-02-08 13:28:09 +0600

    manage user table design added commit

--- src/DashBoard/Manage Users/ManageUsers.jsx
+++ src/DashBoard/Manage Users/ManageUsers.jsx
@@ -1,9 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/UseAxiosSecure/UseAxiosSecure";
+import { FaBan } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 
 const ManageUsers = () => {
+
+
+    
+
+
+
     return (
-        <div>
-            <h1>This Is Manage Users</h1>
+        <div className="lg:p-16 min-h-screen">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
+                    <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400 dark:text-white">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Number
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Photo
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Email
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Phone
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Role
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Banned
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };

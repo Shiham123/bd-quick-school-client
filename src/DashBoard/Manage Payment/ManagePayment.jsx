ff68ccb8c4e55c5f995e76a1774fc2182c3f22ed
tonoy3125 <shaifshajedt@gmail.com>
2024-02-08 22:23:15 +0600

    payment table heading design added

--- src/DashBoard/Manage Payment/ManagePayment.jsx
+++ src/DashBoard/Manage Payment/ManagePayment.jsx
@@ -1,11 +1,56 @@
 
 
-const ManagePayment = () => {
+
+
+
+const ManageUsers = () => {
+    
+
+
+
     return (
-        <div>
-            <h1>This is Manage Payment</h1>
+        <div className="lg:p-16 min-h-screen">
+            <h1 className="text-4xl text-center font-cinzel">Manage Users</h1>
+            <hr className="mb-5 border-2 mt-2 border-black w-[280px] mx-auto" />
+            
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+                <table className="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
+                    {/* Table Heading Start Here */}
+                    <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400 dark:text-white">
+                        <tr className="font-cinzel">
+                            <th scope="col" className="px-6 py-3">
+                                Number
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Photo
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Email
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User Phone
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Role
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Banned
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    
+                </table>
+            </div>
+
         </div>
     );
 };
 
-export default ManagePayment;
+export default ManageUsers;

234184f98acad325edcd2e94db6607a26a800677
tonoy3125 <shaifshajedt@gmail.com>
2024-02-10 23:28:39 +0600

    advertise reviews table design and heading added commit

--- src/DashBoard/Advertise Reviews/AdveriseReviews.jsx
+++ src/DashBoard/Advertise Reviews/AdveriseReviews.jsx
@@ -22,7 +22,41 @@ const AdveriseReviews = () => {
 
 
     return (
-        <div>
+        <div className="lg:p-16 min-h-screen">
+            <h1 className="text-4xl text-center font-cinzel">Advertise Reviews</h1>
+            <hr className="mb-5 border-2 mt-2 border-black w-[280px] mx-auto" />
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+                <table className="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
+                    {/* Table Heading Start Here */}
+                    <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400 dark:text-white">
+                        <tr className="font-cinzel">
+                            <th scope="col" className="px-6 py-3">
+                                Number
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Reviewer Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Reviewer Designation
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Reviewer Rating
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Reviewer Text
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Advertise
+                            </th>
+                        </tr>
+                    </thead>
+                    {/* Table Heading End Here */}
+                    {/* Table Data Fetching */}
+
+                </table>
+            </div>
 
         </div>
     );

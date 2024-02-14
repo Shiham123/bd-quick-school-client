94207b55380c0afb85496659b010f81699717836
tonoy3125 <shaifshajedt@gmail.com>
2024-02-14 23:19:25 +0600

    Announcement Sub-Description input field added

--- src/DashBoard/Add Announcement/AddAnnouncement.jsx
+++ src/DashBoard/Add Announcement/AddAnnouncement.jsx
@@ -10,7 +10,7 @@ const AddAnnouncement = () => {
     };
 
     return (
-        <div className="mx-auto px-4">
+        <div className="mx-auto px-4 min-h-screen">
             <h1 className=" text-2xl lg:text-4xl text-center font-cinzel mt-5">Add Annnouncement</h1>
             <hr className="mb-5 border-2 mt-2 border-black w-[300px] lg:w-[430px] mx-auto" />
             <section className="lg:p-28">
@@ -32,7 +32,18 @@ const AddAnnouncement = () => {
                                     id=""
                                 />
                             </div>
-                            
+                            <div className="col-span-full lg:col-span-3">
+                                <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">
+                                    Announcement Sub-Description <span className="text-red-700">*</span>
+                                </h2>
+                                <input
+                                    className="pt-4 pb-4 pl-2 md:p-4 w-full border border-black  text-base font-normal text-[#1B1A1A99] rounded"
+                                    type="text"
+                                    name="title"
+                                    placeholder="Enter Announcement Sub-Description Here"
+                                    id=""
+                                />
+                            </div>
 
 
 

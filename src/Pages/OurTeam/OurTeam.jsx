53d9066d373dd166c58021ca8adb5a85ca54d452
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-14 22:09:36 +0600

    meet section fixed

--- src/Pages/OurTeam/OurTeam.jsx
+++ src/Pages/OurTeam/OurTeam.jsx
@@ -323,7 +323,7 @@ const OurTeam = () => {
 
               <div className="card-actions justify-end">
                 {/* <button className="btn btn-primary">Buy Now</button> */}
-                {/* <button className="btn btn-primary">Buy Now</button> */}
+               
               </div>
             </div>
           </div>
@@ -335,3 +335,6 @@ const OurTeam = () => {
 };
 
 export default OurTeam;
+
+
+{/* <button className="btn btn-primary">Buy Now</button> */ }

cd52da4a5784a3ddf654b2223694c4b8c2505c14
tonoy3125 <shaifshajedt@gmail.com>
2024-02-15 01:43:20 +0600

    manage announcement table design and heading added commit

--- src/DashBoard/ManageAnnouncements/ManageAnnouncements.jsx
+++ src/DashBoard/ManageAnnouncements/ManageAnnouncements.jsx
@@ -2,8 +2,46 @@
 
 const ManageAnnouncements = () => {
     return (
-        <div>
-            
+        <div className="lg:p-16 min-h-screen">
+            <h1 className="text-4xl text-center font-cinzel">Manage Announcement</h1>
+            <hr className="mb-5 border-2 mt-2 border-black w-[280px] mx-auto" />
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+                <table className="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
+                    {/* Table Heading Start Here */}
+                    <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400 dark:text-white">
+                        <tr className="font-cinzel">
+                            <th scope="col" className="px-6 py-3">
+                                Number
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Announcement Title
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Announcement Sub-Description
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                View
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Update
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Delete
+                            </th>
+                        </tr>
+                    </thead>
+                    {/* Table Heading End Here */}
+                    {/* Table Data Fetching */}
+                    {/* <tbody className="font-lora">
+                        {
+                            filteredData.map((user, index) => <ManageUserTable key={user._id} user={user} index={index} refetch={refetch} />)
+                        }
+                    </tbody> */}
+                </table>
+            </div>
+
         </div>
     );
 };

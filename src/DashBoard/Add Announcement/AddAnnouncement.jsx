3a669e561873b3a7f624401776f8720ff4af726f
tonoy3125 <shaifshajedt@gmail.com>
2024-02-14 23:00:05 +0600

    some alignment changes

--- src/DashBoard/Add Announcement/AddAnnouncement.jsx
+++ src/DashBoard/Add Announcement/AddAnnouncement.jsx
@@ -10,21 +10,23 @@ const AddAnnouncement = () => {
     };
 
     return (
-        <div>
+        <div className="mx-auto px-4">
             <h1 className="text-4xl text-center font-cinzel mt-5">Add Annnouncement</h1>
             <hr className="mb-5 border-2 mt-2 border-black w-[430px] mx-auto" />
-            <div className="col-span-full">
-                <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">
-                    Course Outcome <span className="text-red-700">*</span>
-                </h2>
-                <ReactQuill
-                    modules={modules}
-                    className="h-32 mb-12"
-                    value={outcome}
-                    onChange={setOutcome}
-                    theme="snow"
-                />
-            </div>
+            <section className="lg:p-28">
+                <div className="col-span-full">
+                    <h2 className=" text-base md:text-xl font-semibold text-black mb-2 lg:mb-4">
+                        Course Outcome <span className="text-red-700">*</span>
+                    </h2>
+                    <ReactQuill
+                        modules={modules}
+                        className="h-32 mb-12"
+                        value={outcome}
+                        onChange={setOutcome}
+                        theme="snow"
+                    />
+                </div>
+            </section>
         </div>
     );
 };

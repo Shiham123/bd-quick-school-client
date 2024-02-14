997dd679931b76a31293245cb0af91ccaee13493
tonoy3125 <shaifshajedt@gmail.com>
2024-02-15 01:21:42 +0600

    announcement data send to the database and using post method

--- src/DashBoard/Add Announcement/AddAnnouncement.jsx
+++ src/DashBoard/Add Announcement/AddAnnouncement.jsx
@@ -1,8 +1,33 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import ReactQuill from 'react-quill';
+import useAxiosSecure from './../../Hooks/UseAxiosSecure/UseAxiosSecure';
+import toast, { Toaster } from "react-hot-toast";
 
 const AddAnnouncement = () => {
-    const [outcome, setOutcome] = useState('');
+    const [outcome, setOutcome] = useState('')
+    const { register, handleSubmit, reset } = useForm();
+    const axiosSecure = useAxiosSecure()
+
+
+    const onSubmit = async (data) => {
+        // console.log(data)
+        const item = {
+            announcementtitle: data?.announcementtitle,
+            announcemensubdescription: data?.announcemensubdescription,
+            outcome
+
+        }
+        // console.log(item)
+        const itemRes = await axiosSecure.post('/api/v1/admin/announcement', item)
+        // console.log(itemRes)
+        if (itemRes.data.insertedId) {
+            reset()
+            setOutcome(null);
+            toast.success('Your Announcement Have be Added')
+        }
+    }
+
 
     //React quil Modules Design
     const modules = {
@@ -14,7 +39,7 @@ const AddAnnouncement = () => {
             <h1 className=" text-2xl lg:text-4xl text-center font-cinzel mt-10">Add Annnouncement</h1>
             <hr className="mb-5 border-2 mt-2 border-black w-[300px] lg:w-[430px] mx-auto" />
             <section className="lg:p-28">
-                <form className="container flex flex-col mx-auto space-y-12">
+                <form onSubmit={handleSubmit(onSubmit)} className="container flex flex-col mx-auto space-y-12">
                     <fieldset className="grid gap-6  font-lora dark:bg-gray-900">
                         <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
                             {/* Announcement Title */}
@@ -25,8 +50,8 @@ const AddAnnouncement = () => {
                                 <input
                                     className="pt-4 pb-4 pl-2 md:p-4 w-full border border-black  text-base font-normal text-[#1B1A1A99] rounded"
                                     type="text"
-                                    name="title"
-
+                                    name="announcementtitle"
+                                    {...register('announcementtitle', { required: true })}
                                     placeholder="Enter Announcement Title Here"
                                     id=""
                                 />
@@ -40,6 +65,7 @@ const AddAnnouncement = () => {
                                     className="pt-4 pb-4 pl-2 md:p-4 w-full border border-black  text-base font-normal text-[#1B1A1A99] rounded"
                                     type="text"
                                     name="title"
+                                    {...register('announcemensubdescription', { required: true })}
                                     placeholder="Enter Announcement Sub-Description Here"
                                     id=""
                                 />
@@ -59,8 +85,17 @@ const AddAnnouncement = () => {
                             </div>
                         </div>
                     </fieldset>
+                    {/* Button */}
+                    <div className="col-span-full mt-5">
+                        <input
+                            type="submit"
+                            value="Submit Announcement"
+                            className="btn btn-block bg-[#4357AD] text-lg text-[#fff] hover:bg-[#154360] "
+                        />
+                    </div>
                 </form>
             </section>
+            <Toaster />
         </div>
     );
 };

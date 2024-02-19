d6fa6901c92321e2b068dd3bff3b85974cf3a8e4
tonoy3125 <shaifshajedt@gmail.com>
2024-02-19 22:59:49 +0600

    tablet mode responsive readded

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -26,21 +26,21 @@ const Services = () => {
             />
           </h2>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 md:gap-5 lg:gap-10 py-10">
             {data?.map((service) => (
               <Link to={`/ServiceDetails/${service._id}`} key={service._id}>
                 <div
                   key={service.Id}
-                  className="cursor-pointer  text-white rounded-xl shadow-2xl border"
+                  className="cursor-pointer  text-white rounded-xl shadow-2xl shadow-[#2980b9] border"
                 >
                   <img className="w-full  rounded-t-xl  " src={service.image} alt="" />
                   <div className="p-4">
-                    <h2 className="text-3xl font-bold py-6 font-cinzel">{service.title}</h2>
-                    <p className="text-lg md:text-xl font-lora">{service.shortdescription}</p>
+                    <h2 className=" md:text-2xl lg:text-3xl font-bold py-6 font-cinzel">{service.title}</h2>
+                    <p className=" text-lg  font-lora">{service.shortdescription}</p>
                     <div className="flex justify-between items-center mb-2 font-lora">
                       <div className="flex items-center justify-start gap-2">
                         <img className="w-[30px] h-[30px] rounded-full" src={service.teacherImage} alt="" />
-                        <p className="text-lg md:text-2xl font-bold">{service.teachername}</p>
+                        <p className="text-lg lg:text-2xl font-bold">{service.teachername}</p>
                       </div>
 
                       <p className="py-4 text-lg md:text-2xl font-bold">Price: ${service.price}</p>

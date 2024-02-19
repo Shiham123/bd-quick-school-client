1a2703960aa91a77aadc8f605a44352e54524793
tonoy3125 <shaifshajedt@gmail.com>
2024-02-19 22:32:37 +0600

    our service card laptop device reponsive readded and font added commit

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
   return (
     <div>
       <Services_banner></Services_banner>
-      <div className="py-16 w-[90%] md:w-[75%] mx-auto text-white">
+      <div className="py-16 max-w-7xl mx-auto text-white">
         {isLoading ? (
           <h2 className="flex justify-center items-center min-h-[60vh]">
             <ThreeCircles
@@ -26,18 +26,18 @@ const Services = () => {
             />
           </h2>
         ) : (
-          <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-x-10 gap-y-10 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-10">
             {data?.map((service) => (
               <Link to={`/ServiceDetails/${service._id}`} key={service._id}>
                 <div
                   key={service.Id}
-                  className="cursor-pointer bg-white text-black rounded-xl shadow-2xl border-b-8 border-borderColorOne transition duration-600 ease-in-out hover:scale-105"
+                  className="cursor-pointer  text-white rounded-xl shadow-2xl border"
                 >
-                  <img className="w-[100%] h-[260px] rounded-t-xl border-t " src={service.image} alt="" />
+                  <img className="w-full  rounded-t-xl  " src={service.image} alt="" />
                   <div className="p-4">
-                    <h2 className="text-3xl font-bold py-6">{service.title}</h2>
-                    <p className="text-lg md:text-xl">{service.shortdescription}</p>
-                    <div className="flex justify-between items-center mb-2">
+                    <h2 className="text-3xl font-bold py-6 font-cinzel">{service.title}</h2>
+                    <p className="text-lg md:text-xl font-lora">{service.shortdescription}</p>
+                    <div className="flex justify-between items-center mb-2 font-lora">
                       <div className="flex items-center justify-start gap-2">
                         <img className="w-[30px] h-[30px] rounded-full" src={service.teacherImage} alt="" />
                         <p className="text-lg md:text-2xl font-bold">{service.teachername}</p>
@@ -46,7 +46,7 @@ const Services = () => {
                       <p className="py-4 text-lg md:text-2xl font-bold">Price: ${service.price}</p>
                     </div>
 
-                    <button className="bg-servicesBg text-white font-bold py-2 px-4 border-b-4 border-borderColorOne hover:border-black rounded">
+                    <button className=" text-white font-bold py-2 px-4 border rounded font-cinzel">
                       Explore
                     </button>
                   </div>

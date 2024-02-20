72b325f533384987f2a3fb30e77651bb87bc42b3
tonoy3125 <shaifshajedt@gmail.com>
2024-02-20 19:18:29 +0600

    data get by email with tanstack query and using get method

--- src/Pages/Payment History/PaymentHistory.jsx
+++ src/Pages/Payment History/PaymentHistory.jsx
@@ -1,8 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../Hooks/useAuth/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic/useAxiosPublic";
 
 const PaymentHistory = () => {
+
+    const { user } = useAuth()
+    const axiosPublic = useAxiosPublic()
+    const { data: payments = [], refetch } = useQuery({
+        queryKey: ['payments', user?.email],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/api/v1/user/order/payment/history/${user?.email}`)
+            // console.log(res.data)
+            return res.data
+        }
+    })
+
+
     return (
         <div>
-            
+
         </div>
     );
 };

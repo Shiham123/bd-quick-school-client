142a326c3ecaa0dffc5ec713dd4c790d4b1b6c48
tonoy3125 <shaifshajedt@gmail.com>
2024-02-20 19:38:03 +0600

    table heading added and basic table design added

--- src/Pages/Payment History/PaymentHistory.jsx
+++ src/Pages/Payment History/PaymentHistory.jsx
@@ -17,8 +17,24 @@ const PaymentHistory = () => {
 
 
     return (
-        <div>
+        <div className="container mx-auto">
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className="text-white text-xl">Number</th>
+                            <th className="text-white text-xl">Title</th>
+                            <th className="text-white text-xl">Name</th>
+                            <th className="text-white text-xl">Email</th>
+                            <th className="text-white text-xl">Price</th>
+                            <th className="text-white text-xl">Transjection Id</th>
+                            <th className="text-white text-xl">Time</th>
+                        </tr>
+                    </thead>
 
+                </table>
+            </div>
         </div>
     );
 };

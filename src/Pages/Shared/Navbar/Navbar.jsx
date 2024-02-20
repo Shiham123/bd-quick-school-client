68f52d78192cb8eca11107f64bbfed9f048d1792
tonoy3125 <shaifshajedt@gmail.com>
2024-02-20 14:53:50 +0600

    payment history route added commit

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -347,9 +347,11 @@ const Navbar = () => {
                     Certificate
                   </li>
                   <hr />
-                  <li className="hover:font-semibold py-1  text-start font-lora font-medium hover:text-[#ffbe0b] mb-1 mt-2 hover:translate-x-4 hover:ease-out hover:duration-1000">
-                    Payment History
-                  </li>
+                  <Link to='/paymenthistory'>
+                    <li className="hover:font-semibold py-1  text-start font-lora font-medium hover:text-[#ffbe0b] mb-1 mt-2 hover:translate-x-4 hover:ease-out hover:duration-1000">
+                      Payment History
+                    </li>
+                  </Link>
                   <hr />
                   <li className="hover:font-semibold py-1  text-start font-lora font-medium hover:text-[#ffbe0b] mb-1 mt-2 hover:translate-x-4 hover:ease-out hover:duration-1000">
                     Payment Management

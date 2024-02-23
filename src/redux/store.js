f7b4bebcd05e6a5b9b361f7d18391e7f074c1189
tonoy3125 <shaifshajedt@gmail.com>
2024-02-23 20:56:41 +0600

    store configure added commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,18 +3,25 @@ import { addServicesApi } from './services/ServicesApiSlice';
 import { quizeApiServices } from './services/QuizeApiSlice';
 import { orderApiSlice } from './services/OrderApiSlice';
 
+import { announcementsApi } from './Announcement/announcementsApi';
+
+
 const store = configureStore({
   reducer: {
     [addServicesApi.reducerPath]: addServicesApi.reducer,
     [quizeApiServices.reducerPath]: quizeApiServices.reducer,
     [orderApiSlice.reducerPath]: orderApiSlice.reducer,
+    // [addCourseVideoAPi.reducerPath]: addCourseVideoAPi.reducer,
+    [announcementsApi.reducerPath]: announcementsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       addServicesApi.middleware,
       quizeApiServices.middleware,
       orderApiSlice.middleware,
+      announcementsApi.middleware,
     ]),
 });
 
 export default store;
+

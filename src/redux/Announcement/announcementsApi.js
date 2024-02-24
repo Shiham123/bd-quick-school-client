895fca997e71f42c4d07e555e6187ed701b4b445
Subroto Das <rupashi.bangla.club@gmail.com>
2024-02-24 15:17:03 +0600

    this branch first code push

--- src/redux/Announcement/announcementsApi.js
+++ src/redux/Announcement/announcementsApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// https://quiz-school-server.vercel.app
+// https://quiz-school-server.vercel.app //
 
 export const announcementsApi = createApi({
   reducerPath: 'announcementsApi',

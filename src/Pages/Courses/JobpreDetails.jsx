7e1d4e9ead0080d432bd30166e6ab2c8ba4668a9
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-25 16:57:34 +0600

    job details page design

--- src/Pages/Courses/JobpreDetails.jsx
+++ src/Pages/Courses/JobpreDetails.jsx
@@ -16,13 +16,13 @@ const JobpreDetails = () => {
   
     return (
         <div>
-            <div className=" w-[1100px] border-4 rounded-lg border-black  h-[500px] mt-32 mx-auto flex justify-between gap-2 items-center">
+            <div className=" w-[1100px] border-2 rounded-lg border-black  h-[500px] mt-32 mx-auto flex justify-between gap-2 items-center">
                 <div className="w-[50%] h-full ">
-                    <figure><img className="w-full h-[492px] " src={detailsJob.image} alt="Shoes" /></figure>
+                    <figure><img className="w-full h-[496px] " src={detailsJob.image} alt="Shoes" /></figure>
 
                 </div>
                
-                <div className="w-[50%]  h-[500px] pt-10 px-6">
+                <div className="w-[50%]  h-[500px] pt-10 px-6 text-white">
                     <h2 className="font-bold text-4xl my-5 font-lora ">{detailsJob.title}</h2>
                     <p className="font-medium text-xl mb-2 font-lora">{detailsJob.details}</p>
                     <p className="font-medium text-xl mb-6 font-lora">{detailsJob.outcome}</p>

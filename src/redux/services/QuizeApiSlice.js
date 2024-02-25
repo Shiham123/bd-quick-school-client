30dc1874c5a193f33979103d02eb82a22414222d
Shiham123 <s36651786@gmail.com>
2024-02-25 10:27:48 +0600

    implement redux

---
+++ src/redux/services/QuizeApiSlice.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// https://quiz-school-server.vercel.app
+
+export const quizeApiServices = createApi({
+  reducerPath: 'quizeApiServices',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://quiz-school-server.vercel.app/api/v2',
+  }),
+  endpoints: (builder) => ({
+    //Get Data
+    getAllQUizeUsers: builder.query({
+      query: () => '/quizusers',
+      providesTags: ['quize'],
+    }),
+
+    //
+  }),
+});
+
+export const { useGetAllQUizeUsersQuery } = quizeApiServices;

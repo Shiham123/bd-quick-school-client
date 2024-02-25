3eba9ecee70b3d21b730218b6e605e9d5a845bff
Shiham123 <s36651786@gmail.com>
2024-02-25 10:28:51 +0600

    add redux to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { addServicesApi } from './services/ServicesApiSlice';
+import { quizeApiServices } from './services/QuizeApiSlice';
 import { orderApiSlice } from './services/OrderApiSlice';
 import { addCourseVideoAPi } from './services/VideoApiSlice.js/VideoApiSlice';
 import { announcementsApi } from './Announcement/announcementsApi';
@@ -8,6 +9,7 @@ import { quizApi } from './IsQuizUser/QuizUserSlice';
 const store = configureStore({
   reducer: {
     [addServicesApi.reducerPath]: addServicesApi.reducer,
+    [quizeApiServices.reducerPath]: quizeApiServices.reducer,
     [orderApiSlice.reducerPath]: orderApiSlice.reducer,
     // [addCourseVideoAPi.reducerPath]: addCourseVideoAPi.reducer,
     [announcementsApi.reducerPath]: announcementsApi.reducer,
@@ -17,6 +19,7 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       addServicesApi.middleware,
+      quizeApiServices.middleware,
       orderApiSlice.middleware,
       announcementsApi.middleware,
       addCourseVideoAPi.middleware,

75f3e01b65b5b318592662ba6002f4f11fa5d4f5
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-29 10:18:41 +0600

    job preparation design done

--- src/Pages/Courses/FreeJobPreDetails.jsx
+++ src/Pages/Courses/FreeJobPreDetails.jsx
@@ -8,22 +8,27 @@ const FreeJobPreDetails = () => {
 
     return (
         <div>
-            <div className=" w-[1100px] border-4 rounded-lg border-black  h-[500px] mt-32 mx-auto flex justify-between gap-2 items-center">
-                <div className="w-[50%] h-full ">
-                    <figure><img className="w-full h-[492px] " src={freedetailsJob.image} alt="Shoes" /></figure>
 
-                </div>
 
-                <div className="w-[50%]  h-[500px] pt-28 px-6">
-                    <h2 className="font-bold text-4xl my-6 font-lora ">{freedetailsJob.provider}</h2>
-                    <p className="font-medium text-xl mb-6 font-lora">{freedetailsJob.course}</p>
-                    <p className="font-medium text-xl mb-6 font-lora">Duration : {freedetailsJob.duration}</p>
-                    <p className="font-medium text-xl mb-6 font-lora">{freedetailsJob.certification}</p>
-                    {/* <p className="font-medium text-xl mb-6 font-lora"> Course  : {freedetailsJob.price}</p> */}
-                    {/* <p className="font-medium text-xl mb-6 font-lora"> Teacher Designation : {freedetailsJob.designation}</p> */}
+            <div>
+                <div className="w-[420px] md:w-[720px] lg:w-[1000px] border-2 rounded-lg border-black md:h-[354px]  lg:h-[390px] mt-32 mx-auto md:flex justify-between gap-2 items-center">
+                    <div className="md:w-[50%] h-[300px] md:h-full ">
+                        <figure> <img className="w-full h-[300px] md:h-[350px] lg:h-[386px] " src={freedetailsJob.image} alt="Shoes" /></figure>
 
+                    </div>
+
+                    <div className=" w-full text-center md:text-left md:w-[50%] md:h-[px]  lg:h-[500px] lg:pt-10 md:px-2 lg:px-6 text-white lg:mt-36">
+                        <h2 className="font-bold text-2xl lg:text-4xl my-5 font-lora ">{freedetailsJob.provider}</h2>
+                        <p className="font-medium text-xl md:text-base lg:text-xl mb-2 font-lora">{freedetailsJob.course}</p>
+                        <p className="font-medium text-xl  md:text-base lg:text-xl mb-6 font-lora">Duration : {freedetailsJob.duration}</p>
+                        <p className="font-medium  text-xl md:text-base lg:text-xl mb-4 md:mb-2 lg:mb-2 font-lora"> {freedetailsJob.certification}</p>
+
+
+                    </div>
                 </div>
             </div>
+
+
             
         </div>
     );
--- src/Pages/Courses/JobpreDetails.jsx
+++ src/Pages/Courses/JobpreDetails.jsx
@@ -16,20 +16,20 @@ const JobpreDetails = () => {
   
     return (
         <div>
-            <div className="w-[420px] md:w-[720px] lg:w-[1100px] border-2 rounded-lg border-black md:h-[410px]  lg:h-[500px] mt-32 mx-auto md:flex justify-between gap-2 items-center">
+            <div className="w-[420px] md:w-[720px] lg:w-[1200px] border-2 rounded-lg border-black md:h-[410px]  lg:h-[500px] mt-32 mx-auto md:flex justify-between gap-2 items-center">
                 <div className="md:w-[50%] h-[300px] md:h-full ">
                     <figure> <img className="w-full h-[300px] md:h-[406px] lg:h-[496px] " src={detailsJob.image} alt="Shoes" /></figure>
 
                 </div>
                
-                <div className=" w-full text-center md:w-[50%] md:h-[px]  lg:h-[500px] lg:pt-10 md:px-2 lg:px-6 text-white">
+                <div className=" w-full text-center md:text-left md:w-[50%] md:h-[px]  lg:h-[500px] lg:pt-10 md:px-2 lg:px-6 text-white">
                     <h2 className="font-bold text-2xl lg:text-4xl my-5 font-lora ">{detailsJob.title}</h2>
                     <p className="font-medium text-xl md:text-base lg:text-xl mb-2 font-lora">{detailsJob.details}</p>
                     <p className="font-medium text-xl  md:text-base lg:text-xl mb-6 font-lora">{detailsJob.outcome}</p>
                     <p className="font-medium text-xl md:text-base lg:text-xl mb-6 font-lora">Course Price : ${detailsJob.price}</p>
                     <p className="font-medium text-xl md:text-base lg:text-xl mb-6 font-lora"> Teacher Name : {detailsJob.teachername}</p>
                     <p className="font-medium  text-xl md:text-base lg:text-xl mb-4 md:mb-2 lg:mb-6 font-lora"> Teacher Designation : {detailsJob.designation}</p>
-                    
+
                     
                 </div>
             </div>

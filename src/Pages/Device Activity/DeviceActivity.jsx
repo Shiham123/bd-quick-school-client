053fe24fb19e3f4f1f967e5b14ee9f7fddda08cd
tonoy3125 <shaifshajedt@gmail.com>
2024-02-29 16:47:15 +0600

    border added and some changes commit

--- src/Pages/Device Activity/DeviceActivity.jsx
+++ src/Pages/Device Activity/DeviceActivity.jsx
@@ -3,11 +3,11 @@
 const DeviceActivity = () => {
     return (
         <div>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div className="relative overflow-x-auto shadow-md ">
 
                 <table className="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
                     {/* Table Heading Start Here */}
-                    <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400 dark:text-white">
+                    <thead className="text-xs text-white uppercase   border dark:text-white">
                         <tr className="font-cinzel">
                             <th scope="col" className="px-6 py-3">
                                 Serial
@@ -21,13 +21,13 @@ const DeviceActivity = () => {
                             <th scope="col" className="px-6 py-3">
                                 Action
                             </th>
-                            
+
                         </tr>
                     </thead>
                     {/* Table Heading End Here */}
                     {/* Table Data Fetching */}
                     <tbody className="font-lora">
-
+                        
                     </tbody>
                 </table>
             </div>

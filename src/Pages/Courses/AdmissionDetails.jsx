d6639c21c64c7615a91a02f51c4657618298a71a
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-29 20:20:23 +0600

    addmision test section add techer details

--- src/Pages/Courses/AdmissionDetails.jsx
+++ src/Pages/Courses/AdmissionDetails.jsx
@@ -9,7 +9,65 @@ const AdmissionDetails = () => {
     return (
         <div>
 
-            <div className="w-[420px] md:w-[720px] lg:w-[1200px] border-2 rounded-lg border-black md:h-[410px]  lg:h-[500px] mt-32 mx-auto md:flex justify-between gap-2 items-center">
+
+            {/* course details */}
+            <div className=" w-[420px] md:w-[720px] lg:w-[1200px]  mx-auto">
+                <h1 className="text-white font-bold mt-32 mb-20 md:text-3xl lg:text-4xl font-lora ">Admission Test Course Details</h1>
+
+            </div>
+           
+            <div className="w-[410px] md:w-[720px] lg:w-[1200px]   md:h-[300px] lg:h-[350px]  mx-auto md:flex justify-between items-center mb-32">
+        
+                <div className=" w-[60%]  md:w-[50%]  text-center md:text-left  md:h-[300px] lg:h-[350px]   text-white ">
+                    <h2 className="font-bold md:text-xl lg:text-3xl mb-5 font-lora ">{detailsAdmission.title}</h2>
+                    <p className="font-medium text-xl md:text-base lg:text-xl mb-2 font-lora">{detailsAdmission.details}</p>
+                    <p className="font-medium text-xl  md:text-base lg:text-xl mb-6 font-lora">{detailsAdmission.outcome}</p>
+                    <p className="font-medium text-xl md:text-base lg:text-xl mb-6 font-lora">{detailsAdmission.shortdescription}</p>
+                    <p className="font-medium text-xl md:text-base lg:text-xl mb-6 font-lora">Course Price : ${detailsAdmission.price}</p>
+                    
+                </div>
+
+                <div className="md:w-[300px] lg:w-[500px] md:h-[300px] lg:h-[350px]">
+                    <figure> <img className="w-full h-[300px] md:h-[300px] lg:h-[350px] rounded-lg" src={detailsAdmission.image} alt="Shoes" /></figure>
+
+                </div>
+            </div>
+
+
+            {/* teacher details */}
+
+
+
+            <div className=" w-[420px] md:w-[720px] lg:w-[1200px]  mx-auto">
+                <h1 className="text-white font-bold mt-22 mb-10 text-4xl font-lora">Teacher Details</h1>
+
+            </div>
+
+            <div className="w-[420px] md:w-[720px] lg:w-[1200px]   md:h-[410px]  lg:h-[500px]   md:flex justify-start md:gap-10 lg:gap-20  mx-auto">
+
+               
+
+                <div className="  ">
+                    <figure> <img className="md:w-[300px] h-[300px] md:h-[190px] lg:h-[200px] rounded-lg" src={detailsAdmission.teacherImage} alt="Shoes" /></figure>
+
+                </div>
+
+                <div className=" w-[500px]    text-center md:text-left md:w-[50%] md:h-[px]  lg:h-[500px]   text-white">
+                    <h2 className="font-bold text-2xl lg:text-3xl mb-2 font-lora ">{detailsAdmission.teachername}</h2>
+                    <p className="font-medium text-xl md:text-lg lg:text-xl mb-2 font-lora">{detailsAdmission.designation}</p>
+
+                    <p className="font-medium text-xl md:text-lg lg:text-xl mt-6 mb-2 font-lora">Education :</p>
+
+                    <p className="font-medium text-xl  md:text-base lg:text-xl mb-2 font-lora">{detailsAdmission.graduation}</p>
+                    <p className="font-medium text-xl md:text-base lg:text-xl mb-6 font-lora">{detailsAdmission.postgraduation}</p>
+                    
+
+                </div>
+            </div>
+
+
+
+            {/* <div className="w-[420px] md:w-[720px] lg:w-[1200px] border-2 rounded-lg border-black md:h-[410px]  lg:h-[500px] mt-32 mx-auto md:flex justify-between gap-2 items-center">
                 <div className="md:w-[50%] h-[300px] md:h-full ">
                     <figure> <img className="w-full h-[300px] md:h-[406px] lg:h-[496px] " src={detailsAdmission.image} alt="Shoes" /></figure>
 
@@ -25,7 +83,8 @@ const AdmissionDetails = () => {
 
 
                 </div>
-            </div>
+            </div> */}
+
 
 
            

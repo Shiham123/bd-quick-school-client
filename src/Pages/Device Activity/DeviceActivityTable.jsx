c17f36fdf4ca61d6bb48468bb3ce00eb3846fae0
tonoy3125 <shaifshajedt@gmail.com>
2024-03-01 18:06:54 +0600

    Parse the date string using Moment.js

--- src/Pages/Device Activity/DeviceActivityTable.jsx
+++ src/Pages/Device Activity/DeviceActivityTable.jsx
@@ -1,6 +1,13 @@
+import moment from "moment";
 
 
 const DeviceActivityTable = ({ item, index }) => {
+    // Parse the date string using Moment.js
+    const date = moment(item.date, 'YYYY MM DD HH mm');
+
+    // Format the date in the desired format
+    const formattedDate = date.format('DD-MM-YYYY hh:mm A');
+
     return (
         <tr className=" border-b  ">
             <th scope="row" className="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
@@ -14,11 +21,9 @@ const DeviceActivityTable = ({ item, index }) => {
                 {item.browser}
             </td>
             <td className="px-6 py-4">
-                {item.date}
+                {formattedDate}
             </td>
-            <td className="px-6 py-4">
-                
-            </td>
+
 
 
 

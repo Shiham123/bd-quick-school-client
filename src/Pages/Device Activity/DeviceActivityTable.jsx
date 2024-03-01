6ff7d1eb456d6231461ec46187f3c7427478fd93
tonoy3125 <shaifshajedt@gmail.com>
2024-03-01 18:39:52 +0600

    delete handleactivity function added and onclick specific id delete commit

--- src/Pages/Device Activity/DeviceActivityTable.jsx
+++ src/Pages/Device Activity/DeviceActivityTable.jsx
@@ -1,7 +1,57 @@
 import moment from "moment";
+import useAuth from "../../Hooks/useAuth/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic/useAxiosPublic";
+import Swal from "sweetalert2";
 
 
-const DeviceActivityTable = ({ item, index }) => {
+const DeviceActivityTable = ({ item, index, refetch }) => {
+    const { user, logOut } = useAuth();
+    const axiosPublic = useAxiosPublic()
+
+
+
+
+    // Handle logout 
+    const handleLogOut = () => {
+        logOut()
+            .then((result) => {
+                console.log(result.user);
+                handleActivityDelete(result.user._id)
+            })
+            .then((error) => {
+                console.log(error);
+            });
+    };
+
+
+    const handleActivityDelete = itemId => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosPublic.delete(`/api/v1/device/${itemId}`)
+                    .then(res => {
+                        // console.log(res.data)
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
+
     // Parse the date string using Moment.js
     const date = moment(item.date, 'YYYY MM DD HH mm');
 
@@ -25,7 +75,7 @@ const DeviceActivityTable = ({ item, index }) => {
                 {formattedDate}
             </td>
             <td className="px-6 py-4">
-                <button className="">Remove</button>
+                <button onClick={() => handleActivityDelete(item._id)} className="">Remove</button>
             </td>
 
 

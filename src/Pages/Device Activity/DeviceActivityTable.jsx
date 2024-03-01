c844c17d9b8a43b1b2aaef662ca0d999db0c4b46
tonoy3125 <shaifshajedt@gmail.com>
2024-03-01 12:32:23 +0600

    device activity table added

--- src/Pages/Device Activity/DeviceActivityTable.jsx
+++ src/Pages/Device Activity/DeviceActivityTable.jsx
@@ -2,7 +2,27 @@
 
 const DeviceActivityTable = ({ item, index }) => {
     return (
-        
+        <tr className="border border-b  ">
+            <th scope="row" className="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100 border-collapse border">
+                {index + 1}
+            </th>
+
+            <td className="px-6 py-4 border-collapse border">
+                {item.os}
+            </td>
+            <td className="px-6 py-4 border border-collapse">
+                {item.browser}
+            </td>
+            <td className="px-6 py-4 border border-collapse">
+                {item.date}
+            </td>
+            <td className="px-6 py-4 border border-collapse">
+                
+            </td>
+
+
+
+        </tr>
     );
 };
 

90d5a65aeb72e5f0767b277176e64973df97ed9e
shahisrail <shahisrail134@gmail.com>
2024-03-07 10:46:31 +0600

    some responsive and some dezine in helpdesk

--- src/UserDashbord/HealpDeskShow.jsx
+++ src/UserDashbord/HealpDeskShow.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable no-unused-vars */
+import {  useState } from 'react';
 import useAuth from '../Hooks/useAuth/useAuth';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
+import { FaRegClock } from "react-icons/fa";
+import { FaRegCommentDots } from "react-icons/fa6";
 import {
   useAddCommentPostMutation,
   useGetAllHelpPostQuery,
@@ -43,7 +46,7 @@ const HelpDeskShow = () => {
   //   const fetchComments = async () => {
   //     if (selectedPost) {
   //       try {
-  //         const commentResponse = await axiosSecure.get(`/api/v1/CommentRoutes/${selectedPost._id}`);
+  //         const commentResponse = await axiosSecure.get(/api/v1/CommentRoutes/${selectedPost._id});
   //         setComments(commentResponse.data);
   //         console.log(commentResponse.data);
   //       } catch (error) {
@@ -78,7 +81,8 @@ const HelpDeskShow = () => {
       const yearsDifference = Math.floor(secondsDifference / 31536000);
       return `${yearsDifference} year${yearsDifference !== 1 ? 's' : ''} ago`;
     }
-  };
+};
+
   // make a modal for user comment
 
   const openModal = () => {
@@ -113,22 +117,52 @@ const HelpDeskShow = () => {
 
   return (
     <>
-      <div onClick={openModal}>
+      <div onClick={openModal} className='border rounded-lg border-[#e5e7eb] shadow-2xl xs:max-w-[300px] sm:max-w-[350px] semi-sm:max-w-[400px] md:max-w-[750px] lg:max-w-6xl mx-auto'>
         {posts?.map((post, index) => (
           <div key={index} onClick={() => setSelectedPost(post._id)} className="post">
-            <div className="card max-w-[1300px] border rounded-xl  bg-[#FFFFFF] border-primary-500 mx-auto shadow-xl">
-              <div className="card-body">
-                <div className="flex justify-center items-center gap-5">
-                  <img className="rounded-full w-[48px] h-[48px]" src={user?.photoURL} alt="" />
-                  <p> {user?.displayName}</p>
+            <div className="card ">
+              <div className="card-body text-white">
+                <div className='flex items-center justify-between'>
+                  <div className="flex  items-center gap-5">
+                    <img className="rounded-full w-12 h-12" src={user?.photoURL} alt="" />
+                    <div>
+                      <p className='text-base font-cinzel font-bold' style={{ whiteSpace: 'nowrap' }}> {user?.displayName}</p>
+                      <span className='flex items-center gap-2 font-lora text-xs'>
+                        <FaRegClock />
+                        <p>{formatTimeDifference(post.date)}</p>
+                      </span>
+                    </div>
+                  </div>
+                  <div className='flex items-center gap-2 font-cinzel'>
+                    <button className='btn btn-sm hidden md:block'>Announcement</button>
+                    <button className='flex items-center gap-2'>
+                      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 256 256" className="text-sm hidden md:block" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M238.15,78.54,177.46,17.86a20,20,0,0,0-28.3,0L97.2,70c-12.43-3.33-36.68-5.72-61.74,14.5a20,20,0,0,0-1.6,29.73l45.46,45.47-39.8,39.8a12,12,0,0,0,17,17l39.8-39.81,45.47,45.46A20,20,0,0,0,155.91,228c.46,0,.93,0,1.4-.05A20,20,0,0,0,171.87,220c4.69-6.23,11-16.13,14.44-28s3.45-22.88.16-33.4l51.7-51.87A20,20,0,0,0,238.15,78.54Zm-74.26,68.79a12,12,0,0,0-2.23,13.84c3.43,6.86,6.9,21-6.28,40.65L54.08,100.53c21.09-14.59,39.53-6.64,41-6a11.67,11.67,0,0,0,13.81-2.29l54.43-54.61,55,55Z"></path></svg>
+                      <p className='hidden md:block'>Pinned</p>
+                    </button>
+                  </div>
                 </div>
-                <p>{formatTimeDifference(post.date)}</p>
-                <h2 className="card-title">{post.title}</h2>
-                <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
-                <p>{comments?.filter((comment) => comment?.postId === post._id).length} comments</p>
+
+                <h2 className="text-2xl font-semibold font-cinzel  mt-4">{post.title}</h2>
+                <div className='font-lora' dangerouslySetInnerHTML={{ __html: post.content }}></div>
+                <div className='flex items-center semi-sm:gap-6 md:gap-0 justify-between mt-6'>
+                  <span className='flex items-center gap-2'>
+                    <FaRegCommentDots />
+                    <p>{comments?.filter((comment) => comment?.postId === post._id).length} comments</p>
+                  </span>
+                  <div className='flex items-center gap-2 font-cinzel'>
+                    <button className='btn btn-sm btn-outline border-white text-white hidden md:block'>Website</button>
+                    <div className='hidden sm:block'>
+                      <button className='flex items-center gap-2 btn btn-sm btn-outline border-white text-white'>
+                        <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" aria-hidden="true" className="text-sm" color="#0885c4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" ><path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"></path></svg>
+                        Suggestion
+                      </button>
+                    </div>
+                  </div>
+                </div>
                 {/* show comment data  */}
               </div>
             </div>
+            <hr />
           </div>
         ))}
       </div>
@@ -136,7 +170,7 @@ const HelpDeskShow = () => {
         <div className="fixed inset-0 z-50 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen">
             <div className="fixed inset-0 bg-black opacity-50" onClick={closeModal}></div>
-            <div className="relative bg-white rounded-lg w-full md:w-1/2 mx-auto p-6">
+            <div className="relative bg-white rounded-lg xs:w-[300px] sm:w-[350px] semi-sm:w-[400px] md:w-1/2 mx-auto p-6">
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={closeModal}>
                 ✕
               </button>
@@ -156,7 +190,7 @@ const HelpDeskShow = () => {
                     <ul>
                       {comments?.map((comment, index) => (
                         <li key={index}>
-                          <div className="flex  items-center gap-5">
+                          <div className="flex items-center gap-5">
                             <img className="rounded-full w-[48px] h-[48px]" src={user?.photoURL} alt="" />
                             <p> {user?.displayName}</p>
                             <p>{formatTimeDifference(comment?.date)}</p>
--- src/UserDashbord/HelpDask.jsx
+++ src/UserDashbord/HelpDask.jsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import useAuth from '../Hooks/useAuth/useAuth';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { AiFillPicture } from "react-icons/ai";
 
 import toast from 'react-hot-toast';
 import HelpDeskShow from './HealpDeskShow';
@@ -15,8 +16,8 @@ const HelpDask = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
 
-// post i use redux 
-const [addHelpDesk] = useAddHelpPostMutation() 
+  // post i use redux 
+  const [addHelpDesk] = useAddHelpPostMutation()
 
   const openModal = () => {
     setModalOpen(true);
@@ -36,13 +37,13 @@ const [addHelpDesk] = useAddHelpPostMutation()
     };
 
     try {
-      addHelpDesk(newPost).unwrap().then(()=>{
+      addHelpDesk(newPost).unwrap().then(() => {
         setOutcome('');
         reset();
         toast.success('Your Announcement has been Added');
         closeModal();
       })
-    
+
     } catch (error) {
       console.error('Error submitting post:', error);
     }
@@ -60,20 +61,20 @@ const [addHelpDesk] = useAddHelpPostMutation()
   };
   return (
     <>
-      <div className="max-w-[1300px] border shadow-sm rounded-xl p-5 sm:p-7 bg-[#FFFFFF] border-primary-500 mx-auto my-5 ">
+      <div className=" xs:max-w-[300px] sm:max-w-[350px] semi-sm:max-w-[400px] md:max-w-[750px] lg:max-w-6xl border border-white shadow-sm rounded-xl p-5 sm:p-7  border-primary-500 mx-auto my-5 ">
         <div className="flex gap-3 justify-center items-center">
-          <img className="rounded-full w-[48px] h-[48px]" src={user?.photoURL} alt="" />
+          <img className="rounded-full w-12 h-12" src={user?.photoURL} alt="" />
           <input
             type="text"
             onClick={openModal}
             placeholder="Share or Ask Something to Everyone?"
-            className="input input-bordered rounded-full w-full"
+            className="py-3 px-5 border rounded-full w-full bg-transparent text-white font-cinzel"
           />
           {modalOpen && (
             <div className="fixed inset-0 z-50 overflow-y-auto">
               <div className="flex items-center justify-center min-h-screen">
                 <div className="fixed inset-0 bg-black opacity-50" onClick={closeModal}></div>
-                <div className="relative bg-white rounded-lg w-full md:w-1/2 mx-auto p-6">
+                <div className="relative bg-white rounded-lg xs:w-[300px] sm:w-[350px] semi-sm:w-[400px] md:w-1/2 mx-auto p-6">
                   <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={closeModal}>
                     ✕
                   </button>
@@ -81,49 +82,49 @@ const [addHelpDesk] = useAddHelpPostMutation()
                     <form onSubmit={handleSubmit(onSubmit)}>
                       <div className="form-control w-full">
                         <label className="label">
-                          <span className="label-text">Title</span>
+                          <span className="label-text font-cinzel">Title</span>
                         </label>
                         <input
                           type="text"
                           placeholder="Title"
                           {...register('title', { required: true })}
-                          className="input input-bordered w-full"
+                          className="input input-bordered w-full font-lora"
                         />
                       </div>
                       <div className="form-control w-full">
                         <label className="label">
-                          <span className="label-text">Platform</span>
+                          <span className="label-text font-cinzel">Platform</span>
                         </label>
                         <input
                           type="text"
                           placeholder="Platform"
                           {...register('platform', { required: true })}
-                          className="input input-bordered w-full"
+                          className="input input-bordered w-full font-lora"
                         />
                       </div>
                       <div className="form-control w-full">
                         <label className="label">
-                          <span className="label-text">Post Type</span>
+                          <span className="label-text font-cinzel">Post Type</span>
                         </label>
                         <input
                           type="text"
                           placeholder="Post Type"
                           {...register('postType', { required: true })}
-                          className="input input-bordered w-full"
+                          className="input input-bordered w-full font-lora"
                         />
                       </div>
                       <div className="col-span-full">
                         <label className="label">
-                          <span className="label-text">Content</span>
+                          <span className="label-text font-cinzel">Content</span>
                         </label>
-                        <ReactQuill modules={modules} className="h-32 mb-12" value={outcome} onChange={setOutcome} theme="snow" />
+                        <ReactQuill modules={modules} className="h-32 mb-12 font-lora" value={outcome} onChange={setOutcome} theme="snow" />
                       </div>
                       <div className="flex justify-between items-center gap-3 mt-3">
-                        <label htmlFor="fileInput" className="custom-file-upload text-[16px]">
+                        <label htmlFor="fileInput" className="custom-file-upload text-[16px] font-cinzel">
                           Photo/Video
                           <input id="fileInput" type="file" style={{ display: 'none' }} />
                         </label>
-                        <button type="submit" className="btn-grad btn text-white bg-gray-700 hidden md:block">
+                        <button type="submit" className="btn-grad btn text-white bg-gray-700 font-cinzel">
                           Create Post
                         </button>
                       </div>
@@ -134,11 +135,14 @@ const [addHelpDesk] = useAddHelpPostMutation()
             </div>
           )}
         </div>
-        <div className="flex justify-between gap-3 mt-3">
-          <button className="btn-grad text-[#EE9E1E] hidden md:block h-[70px]" onClick={openModal}>
-            Video/image
-          </button>
-          <button className="btn-grad btn text-white bg-gray-700  hidden md:block " onClick={openModal}>
+        <div className="flex items-center justify-between gap-3 mt-3">
+          <span className='flex items-center gap-2'>
+            <AiFillPicture className='text-2xl text-white' />
+            <button className="btn-grad text-white font-cinzel h-[70px]" onClick={openModal}>
+              Photo/Video
+            </button>
+          </span>
+          <button className="btn btn-outline text-white  font-cinzel  " onClick={openModal}>
             Create Post
           </button>
         </div>
@@ -148,4 +152,4 @@ const [addHelpDesk] = useAddHelpPostMutation()
   );
 };
 
-export default HelpDask;
+export default HelpDask
